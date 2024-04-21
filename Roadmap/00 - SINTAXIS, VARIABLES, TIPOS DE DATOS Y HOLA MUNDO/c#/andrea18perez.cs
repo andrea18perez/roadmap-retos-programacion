c062fd4f0f3476625020226816a5967af1b21ceb
andrea18perez <167718796+andrea18perez@users.noreply.github.com>
2024-04-21 17:41:02 -0500

    Create andrea18perez.cs

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/andrea18perez.cs
@@ -0,0 +1,38 @@
+// Ejercicio 1
+// Página oficial de C#: https://learn.microsoft.com/es-es/dotnet/csharp/
+
+// Ejercicio 2
+// Comentario en varias lineas
+/*
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ */
+
+// Ejercicio 3
+Variable1 = i;
+Const int Constante2 = 4;
+
+// Ejercicio 4
+// Cadena de texto
+string Texto = "Esto es un string";
+// Número entero
+int i = 4;
+// Decimal 32 bits
+float dec = 3.14;
+// Decimal 64 bits
+double decimal = 3,14166453;
+// Verdadero o falso
+bool acceso = true;
+
+// Ejercicio 5
+Main;
+{
+  Const string Language = "C#";
+  Console.Writeline("¡Hola, Language()!");
+}
